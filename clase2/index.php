08731a6b3849cbad425007f051c8e80946c205b3
danrigoli <danterigoli@gmail.com>
2018-08-03 20:59:54 -0300

    cambio index.php

--- clase2/index.php
+++ clase2/index.php
@@ -1,34 +1,33 @@
 <?php
-$dia = ["Lunes", "Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+$dias = ["Lunes", "Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
 $evaluaciones = ["Regular", "Bueno", "Excelente"];
 $nombre = "Daniel Fuentes";
 $dni = 12345678;
 
-
-$evaluacion = 2;
+$dia = 0;
+$evaluacion = 0;
 
 if($evaluaciones[$evaluacion] == "Regular"){
-   $aumento = 10;
+   $porcentaje = 10;
 //    var_dump($aumento);
 //    exit;
 }
 elseif($evaluaciones[$evaluacion] == "Bueno"){
-    $aumento = 20;
+    $porcentaje = 20;
     // var_dump($aumento);
     // exit;
 }
 else{
-    $aumento = 30;
+    $porcentaje = 30;
     //   var_dump($aumento);
     // exit;
 }
 
-$sueldo = 8000 + $aumento * 80;
-
-$aumentob = $aumento * 8000 / 100;
+$sueldo = 8000 + $porcentaje * 80;
+$aumento = $porcentaje * 8000 / 100;
 
 if($evaluacion == 0 || 1 || 2){
- echo "Estimado(a)" . " " . $nombre . ", portador del DNI: " . $dni . "," . "le informamos que el dia de hoy:" . $dia[1] . ", " . "de acuerdo al nivel de evaluacion obtenido:" . $evaluaciones[2] . " " . ",usted ha recibido un aumento del" . ",siendo su aumento de:" . $aumento . "%" . ",siendo su aumento de:" . $aumentob . " y su nuevo sueldo es de: " . $sueldo;
+ echo "Estimado(a)" . " " . $nombre . ", portador del DNI: " . $dni . "," . "le informamos que el dia de hoy: " . $dias[$dia] . ", " . "de acuerdo al nivel de evaluacion obtenido: " . $evaluaciones[$evaluacion] . " " . ",usted ha recibido un aumento del" . ",siendo su aumento de:" . $porcentaje . "%" . ",siendo su aumento de:" . $aumento . " y su nuevo sueldo es de: " . $sueldo;
 
 echo "<hr>";
 
